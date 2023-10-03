0f415f2441c3b773e385b04a794e66d574631545
BearmanAdmin <william.k97430@gmail.com>
2023-10-03 11:57:06 +0200

    init back

---
+++ back/src/main.ts
@@ -0,0 +1,24 @@
+import express, {Express} from 'express';
+import {corsApp} from "./cors/corsApp";
+const port: number = 3000;
+const app = express();
+app.use(express.json());
+const httpServer: Express = require('http').createServer(app);
+const io = require('socket.io')(httpServer, corsApp);
+
+
+io.on('connection', (socket: { on: (arg0: string, arg1: (message: string) => void) => void; id: string; }) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected!');
+    });
+});
+
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
