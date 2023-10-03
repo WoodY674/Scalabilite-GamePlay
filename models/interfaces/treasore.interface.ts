9ec1f931565e20c305868575a20437dd9e2e41e2
msagres <m_costa3@etu-webschoolfactory.fr>
2023-10-03 17:06:35 +0200

    feat: introduction to phaser

---
+++ models/interfaces/treasore.interface.ts
@@ -0,0 +1,4 @@
+export interface Treasure {
+	posX: string
+	posY: string
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
 import type { NextPage } from 'next'
 import React from 'react'
+import Game from '@/components/Game'
 
 const Home: NextPage = () => {
+	const gameData = {
+		mapBackground: '#00FFFF',
+		width: '800',
+		height: '800',
+		treasures: [
+			{
+				posX: '100',
+				posY: '100',
+			},
+			{
+				posX: '200',
+				posY: '200',
+			},
+			{
+				posX: '300',
+				posY: '300',
+			},
+			// Add more treasures as needed
+		],
+	}
+
 	return (
 		<>
-			<h1>home</h1>
+			<h1>Pixel Game</h1>
+			<Game gameData={gameData} />
 		</>
 	)
 }
