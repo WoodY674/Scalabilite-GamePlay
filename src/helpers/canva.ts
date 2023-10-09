6bfe445839e5681c775711107d65bfa42f00cc94
gwenael <gwenael.marchetti-waternaux@edu.itescia.fr>
2023-10-09 04:41:53 +0200

    canvas helper add some fix
    checkImage can lead to CORS issue, so add a try catch to avoid an error on screen.
    add an src to ImageCanvas because trying to get the src of the img element cause unexpected issue.

--- src/helpers/canva.ts
+++ src/helpers/canva.ts
@@ -26,26 +26,38 @@ export function getNotEmpty(val:any, defaultVal:any){
 
 
 export async function checkImage(url:string){
-    const res = await fetch(url);
-    const buff = await res.blob();
-    return buff.type.startsWith('image/')
+    try {
+        const res = await fetch( url );
+        const buff = await res.blob();
+        return buff.type.startsWith( 'image/' )
+    }catch ( e ){
+        console.log("an error occured when check image", e)
+    }
+    return false
 }
 
 export class ImageCanva{
     defaultImg: string
+    src: string
     img: HTMLImageElement
 
     constructor(src:string, defaultImg: string) {
         this.defaultImg = defaultImg
-        this.img = new Image()
-
+        this.src = src
         try {
-            checkImage(this.img.src).then((res)=>{
-                this.img.src =res ? src : this.defaultImg
-            })
+            if(src == "" || src == null){
+                this.src = this.defaultImg
+            }
+            else{
+                checkImage(src).then((res)=>{
+                    this.src = res ? src : this.defaultImg
+                })
+            }
         }catch (e){
             console.log("src error", e)
         }
+        this.img = new Image()
+        this.img.src = this.src
     }
 
     draw(canvasCtx: CanvasRenderingContext2D, dx: number, dy: number, dw: number, dh: number){
